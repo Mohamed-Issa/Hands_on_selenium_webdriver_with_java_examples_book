a89bfec1aa26e8fe7ec0b75f9aba3f51ede9bda5
Mohamed-Issa <mohamed.hamdy.issa@gmail.com>
2026-03-29 09:57:43 +0200

    WebDriverBuilderNGTest

---
+++ src/test/java/chapter_three/WebDriverBuilderNGTest.java
@@ -0,0 +1,52 @@
+package chapter_three;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.slf4j.Logger;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WebDriverBuilderNGTest {
+	
+	static final Logger log = getLogger(lookup().lookupClass());
+
+	 WebDriver driver;
+	
+	@BeforeClass
+	public void setupClass() {
+		WebDriverManager.chromedriver().setup();
+	}
+	
+	@BeforeMethod
+	public void setup() {
+		//driver = new ChromeDriver();
+		driver = RemoteWebDriver.builder().oneOf(new ChromeOptions()).build();
+		
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	@Test
+	public void test() {
+		String url = "https://bonigarcia.dev/selenium-webdriver-java/";
+		driver.get(url);
+		String title  = driver.getTitle();
+		log.debug("the title of {} is {}.", url, title );
+		
+		assertThat(title).isEqualTo("Hands-On Selenium WebDriver with Java");
+	}
+
+}
