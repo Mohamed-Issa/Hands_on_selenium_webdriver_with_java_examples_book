430f457463b50e8a9ef44285ede0fd13c820015d
Mohamed-Issa <mohamed.hamdy.issa@gmail.com>
2026-04-17 06:27:51 +0200

    test shadow dom

---
+++ src/test/java/chapter_four/TestShadowDom.java
@@ -0,0 +1,55 @@
+package chapter_four;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestShadowDom {
+
+	static final Logger log = getLogger(lookup().lookupClass());
+
+	WebDriver driver;
+
+	@BeforeClass
+	public void setupClass() {
+		WebDriverManager.chromedriver().setup();
+	}
+
+	@BeforeMethod
+	public void setup() {
+		driver = new ChromeDriver();
+	}
+
+//	@AfterMethod
+//	public void tearDown() {
+//		driver.quit();
+//	}
+
+	@Test
+	public void testShadowDom() {
+		String initUrl = "https://bonigarcia.dev/selenium-webdriver-java/shadow-dom.html";
+		driver.get(initUrl);
+
+		WebElement content = driver.findElement(By.id("content"));
+		SearchContext shadowRoot = content.getShadowRoot();
+		WebElement textElement = shadowRoot.findElement(By.cssSelector("p"));
+		assertThat(textElement.getText()).contains("Hello Shadow DOM");
+
+	}
+
+}
