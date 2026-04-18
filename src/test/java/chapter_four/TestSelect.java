bd94900335c5d1cb81b2c34ad4be2491a9914ebc
Mohamed-Issa <mohamed.hamdy.issa@gmail.com>
2026-04-18 06:31:17 +0200

    test Select

---
+++ src/test/java/chapter_four/TestSelect.java
@@ -0,0 +1,54 @@
+package chapter_four;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.slf4j.Logger;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestSelect {
+
+	static final Logger log = getLogger(lookup().lookupClass());
+
+	WebDriver driver;
+
+	@BeforeClass
+	public void setupClass() {
+		WebDriverManager.chromedriver().setup();
+	}
+
+	@BeforeMethod
+	public void setup() {
+		driver = new ChromeDriver();
+	}
+
+//	@AfterMethod
+//	public void tearDown() {
+//		driver.quit();
+//	}
+
+	@Test
+	public void testSelect() {
+		String initUrl = "https://bonigarcia.dev/selenium-webdriver-java/web-form.html";
+		driver.get(initUrl);
+
+		Select select = new Select(driver.findElement(By.name("my-select")));
+		String optionLabel = "Three";
+		select.selectByVisibleText(optionLabel);
+
+		assertThat(select.getFirstSelectedOption().getText()).isEqualTo(optionLabel);
+
+	}
+
+}
